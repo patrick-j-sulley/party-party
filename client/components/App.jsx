d523212ab94bcd714ed71f83a812558166cdff2a
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-09 20:28:02 +1200

    get rid of old crap in App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
 
-import ErrorMessage from './ErrorMessage'
-import LoadSubreddit from './LoadSubreddit'
-import SubredditList from './SubredditList'
-import WaitIndicator from './WaitIndicator'
-
 function App () {
   return (
-    <div className='app'>
-      <ErrorMessage />
-      <LoadSubreddit>
-        <WaitIndicator />
-      </LoadSubreddit>
-      <SubredditList />
+    <div className=''>
     </div>
   )
 }
