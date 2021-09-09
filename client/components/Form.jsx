c110a7c76823f6c3f9227b5b66f29bb1343cfa36
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-09 22:15:15 +1200

    pad out From.jsx

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -4,17 +4,35 @@ import { connect } from 'react-redux'
 // imp actionCreator to dispatch API req.
 
 const Form = ({ dispatch }) => {
-  // const [state, setState] = useState('')
+  // this stuff will need to be replaced with some weird array state to track all of the people's requests
+  const [userName, setUserName] = useState('')
+  const [userIngr, setUserIngr] = useState('')
 
   // funcs to take input and dispatch req.
+  
+  const formSubmit = (e) => {
+    e.preventDefault()
+    console.log(`formSubmit for something with ${userIngr} for a beautiful ${userName}`)
+  }
 
-  // name + ingred input
-  // "+" another person btn
-  // submit "Party PARTY!!" btn
+  const addPerson = () => console.log('addPerson yo')
 
   return (
     <div>
-      <p>hello from Form!!</p>
+      <form onSubmit={formSubmit}>
+        <input
+          type="text"
+          placeholder='name'
+          required
+          onChange={(e) => setUserName(e.target.value)} />
+        <input
+          type="text"
+          placeholder='desired ingredient'
+          required
+          onChange={(e) => setUserIngr(e.target.value)} />
+        <button type='button' onClick={addPerson}>+</button>
+        <button type='submit'>Party PARTY!!</button>
+      </form>
     </div>
   )
 }
