cfce627328ffe360a1536cfcb230ee5b0c51f700
Patrick John Sullivan <17734388+patrick-j-sulley@users.noreply.github.com>
2021-09-09 22:38:05 +1200

    Added Bootstrap CSS stylesheet referencing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 import Form from './Form'
 import Drink from './Drink'
 
-function App () {
+function App() {
   // bit of a title
 
   return (
     <div className=''>
-      <h1>Let's get this party started!!</h1>
-      <Form />
-      <Drink />
+      <div class="row">
+        <h1>Let's get this party started!!</h1>
+        <div class="col-md-4">
+          <Form />
+        </div>
+        <Drink />
+      </div>
     </div>
   )
 }
