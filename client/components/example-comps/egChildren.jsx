9bff93fcca46bd9c70479fb2feb4f623fd0dcabc
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-09 20:26:35 +1200

    save some example components

--- client/components/example-comps/egChildren.jsx
+++ client/components/example-comps/egChildren.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { fetchPosts } from '../actions'
+import { fetchPosts } from '../../actions'
 
 function LoadSubreddit ({ children, dispatch }) {
   return (
