90c8c8ed30b042ffb29d52c49a9ca6e0d734409c
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-09 22:30:19 +1200

    pad out Drink.jsx

--- client/components/Drink.jsx
+++ client/components/Drink.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const Drink = ({}) => {
-  // img of drink
-  // drink name
-  // drinker name
-  // ingreds
-
   return (
-    <div>
-      <p>hello from Drink!!</p>
+    <div classNam =''>
+      <img src="https://cocktailpartyapp.com/wp-content/uploads/Halekulani.png" alt="Cartoon cocktail colored orange with a pink flower. It might be a Tequlia Sunrise" />
+      <h3>Tequlia Sunrise</h3>
+      <p>Name Namington</p>
+      <ul>
+        <li>Orange Juice</li>
+        <li>Tequila</li>
+        <li>Grenadine</li>
+      </ul>
     </div>
   )
 }
 
 // const mapStateToProps = () => ({})
 
-export default connect(mapStateToProps)(Drink)
+// export default connect(mapStateToProps)(Drink)
+export default Drink
