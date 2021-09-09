09cf52175662a6c1e9df21b5c4e984969377822a
Patrick John Sullivan <17734388+patrick-j-sulley@users.noreply.github.com>
2021-09-10 11:12:42 +1200

    Refactored rows and columns into individual form & drink components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,19 +8,8 @@ function App() {
 
   return (
     <div className='mx-auto'>
-      
-      <div className="row align-items-start">
-        <div className="col">
-        </div>
-        <div className="col">
-          <h1 className="text-center mt-3 p-4 border">Let's get this party started!!</h1>
           <Form />
           <Drink />
-        </div>
-        <div className="col">
-        </div>
-      </div>
-      
     </div>
   )
 }
--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -23,50 +23,59 @@ const Form = ({ dispatch }) => {
   const addPerson = () => console.log('addPerson yo')
 
   return (
-    <div>
-      <form onSubmit={formSubmit}>
-        <div className="row">
-          <div className="input-group d-flex justify-content-center mt-3">
-            <div className="col">
-              <div className="form-floating me-auto">
-                <input
-                  id="floatingInput"
-                  className="form-control form-control-lg"
-                  type="text"
-                  placeholder="name"
-                  required
-                  onChange={e => setUserName(e.target.value)}
-                />
-                <label htmlFor="floatingInput">Name</label>
+    <div className="row align-items-start">
+      <div className="col">
+      </div>
+      <div className="col">
+        <h1 className="text-center mt-3 p-4 border">Let's get this party started!!</h1>
+        <div>
+          <form onSubmit={formSubmit}>
+            <div className="row">
+              <div className="input-group d-flex justify-content-center mt-3">
+                <div className="col">
+                  <div className="form-floating me-auto">
+                    <input
+                      id="floatingInput"
+                      className="form-control form-control-lg"
+                      type="text"
+                      placeholder="name"
+                      required
+                      onChange={e => setUserName(e.target.value)}
+                    />
+                    <label htmlFor="floatingInput">Name</label>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="form-floating ms-auto">
+                    <input
+                      className="form-control form-control-lg "
+                      type="text"
+                      placeholder="desired ingredient"
+                      required
+                      onChange={e => setUserIngr(e.target.value)}
+                    />
+                    <label htmlFor="floatingInput">Desired Ingredient</label>
+                  </div>
+                </div>
+              </div>
+              <div className="input-group d-flex justify-content-center mt-2 mb-2">
+                <button
+                  className="btn btn-lg btn-success me-auto"
+                  type="button"
+                  onClick={addPerson}
+                >
+                  +
+                </button>
+                <button className="btn btn-lg btn-primary ms-auto" type="submit">
+                  Party PARTY!!
+                </button>
               </div>
             </div>
-            <div className="col">
-              <div className="form-floating ms-auto">
-                <input
-                  className="form-control form-control-lg "
-                  type="text"
-                  placeholder="desired ingredient"
-                  required
-                  onChange={e => setUserIngr(e.target.value)}
-                />
-                <label htmlFor="floatingInput">Desired Ingredient</label>
-              </div>
-            </div>
-          </div>
-          <div className="input-group d-flex justify-content-center mt-2 mb-2">
-            <button
-              className="btn btn-lg btn-success me-auto"
-              type="button"
-              onClick={addPerson}
-            >
-              +
-            </button>
-            <button className="btn btn-lg btn-primary ms-auto" type="submit">
-              Party PARTY!!
-            </button>
-          </div>
+          </form>
         </div>
-      </form>
+      </div>
+      <div className="col">
+      </div>
     </div>
   )
 }
