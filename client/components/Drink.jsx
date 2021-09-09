8f0f35a6aa0edef8fe9cac008c311ca852e2a1d9
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-09 21:13:04 +1200

    create and pad out Drink.jsx

---
+++ client/components/Drink.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const Drink = ({}) => {
+  // img of drink
+  // drink name
+  // drinker name
+  // ingreds
+
+  return (
+    <div>
+      <p>hello from Drink!!</p>
+    </div>
+  )
+}
+
+// const mapStateToProps = () => ({})
+
+export default connect(mapStateToProps)(Drink)
